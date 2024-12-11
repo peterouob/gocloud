82e0dbfdb3fdbac5c03db6319c790abe2f25dc2d
林庭楷 <hellolinpeter@gmail.com>
2024-12-11 15:24:59 +0800

    rotation

--- db/memtable/memtable.go
+++ db/memtable/memtable.go
@@ -31,5 +31,37 @@ func NewTree[K comparable, V comparable](comparator Comparator[K]) *Tree[K, V] {
 	return tree
 }
 
-func (tree *Tree[K, V]) leftRotation(node *Node[K, V])  {}
-func (tree *Tree[K, V]) rightRotation(node *Node[K, V]) {}
+func (tree *Tree[K, V]) leftRotation(node *Node[K, V]) {
+	right := node.right
+	node.right = right.left
+	if right.left != tree.leaf {
+		right.left.parent = node
+	}
+	right.parent = node.parent
+	if node.parent == tree.leaf {
+		tree.root = right
+	} else if node == node.left {
+		node.parent.left = right
+	} else {
+		node.parent.right = right
+	}
+	right.left = node
+	node.parent = right
+}
+func (tree *Tree[K, V]) rightRotation(node *Node[K, V]) {
+	left := node.left
+	node.left = left.right
+	if left.right != tree.leaf {
+		left.right.parent = node
+	}
+	left.parent = node.parent
+	if node.parent == tree.leaf {
+		tree.root = left
+	} else if node == node.parent.left {
+		node.parent.left = left
+	} else {
+		node.parent.right = left
+	}
+	left.right = node
+	node.parent = left
+}

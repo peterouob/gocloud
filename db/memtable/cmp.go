5e7fc779bf828ac40a8141bacd8aa9de9f4e266c
林庭楷 <hellolinpeter@gmail.com>
2024-12-11 18:51:19 +0800

    feat:constraint the interface

--- db/memtable/cmp.go
+++ db/memtable/cmp.go
@@ -8,6 +8,8 @@ type Comparator[T any] interface {
 
 type OrderComparator[T cmp.Ordered] struct{}
 
+var _ Comparator[int] = (*OrderComparator[int])(nil)
+
 func (o *OrderComparator[T]) Compare(a T, b T) int {
 	return cmp.Compare[T](a, b)
 }

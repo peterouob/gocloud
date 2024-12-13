1d641003c67a8343469bb805d2d48c0825118c3a
林庭楷 <hellolinpeter@gmail.com>
2024-12-13 16:38:00 +0800

    config

--- db/config/config.go
+++ db/config/config.go
@@ -1,1 +1,23 @@
 package config
+
+type Config struct {
+	Dir                 string
+	MaxLevel            int
+	SstSize             int
+	SstDataBlockSize    int
+	SstFooterSize       int
+	SstBlockTrailerSize int
+	SstRestartInterval  int
+}
+
+func NewConfig(dir string) *Config {
+	return &Config{
+		Dir:                 dir,
+		MaxLevel:            7,
+		SstSize:             4096 * 1024,
+		SstDataBlockSize:    16 * 1024,
+		SstFooterSize:       40,
+		SstBlockTrailerSize: 4,
+		SstRestartInterval:  16,
+	}
+}

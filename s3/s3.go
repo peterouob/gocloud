49aae1280154bd737db165ec2264413f0bfa8c12
林庭楷 <hellolinpeter@gmail.com>
2025-01-07 01:29:55 +0800

    upload to s3 bucket

--- s3/s3.go
+++ s3/s3.go
@@ -1,15 +1,16 @@
 package s3bucket
 
 import (
+	"bufio"
 	"context"
-	"errors"
 	"fmt"
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
 	"io"
+	"log"
 	"os"
-	"strings"
+	"unicode"
 )
 
 type S3File struct {
@@ -29,36 +30,31 @@ func NewClient(ctx context.Context) *s3.Client {
 		panic(err)
 	}
 
-	client := s3.NewFromConfig(cfg)
+	client := s3.NewFromConfig(cfg, func(o *s3.Options) {
+		o.BaseEndpoint = aws.String("https://localhost.localstack.cloud:4566")
+	})
 
 	return client
 }
 
 func (s3file *S3File) UploadFile(ctx context.Context) error {
-	content, err := ReadLocalFile(s3file.File)
-	if err != nil {
-		return errors.New("error in reading local file : " + err.Error())
+
+	if err := readFileAndFilterString(s3file.File, s3file.Key); err != nil {
+		return fmt.Errorf("readFileAndFilterString: %w", err)
 	}
 
 	input := &s3.PutObjectInput{
 		Bucket: aws.String(bucket),
 		Key:    aws.String(s3file.Key),
-		Body:   strings.NewReader(content),
 	}
 
-	_, err = s3file.Client.PutObject(ctx, input)
-	if err != nil {
+	log.Printf("PutObjectInput: %+v", input)
+
+	if _, err := s3file.Client.PutObject(ctx, input); err != nil {
 		return fmt.Errorf("failed to upload file: %w", err)
+
 	}
 	return nil
-}
-
-func ReadLocalFile(filepath string) (string, error) {
-	content, err := os.ReadFile(filepath)
-	if err != nil {
-		return "", fmt.Errorf("failed to read local file: %w", err)
-	}
-	return string(content), nil
 }
 
 func (s3file *S3File) ReadS3File(ctx context.Context) (string, error) {
@@ -79,4 +75,49 @@ func (s3file *S3File) ReadS3File(ctx context.Context) (string, error) {
 	}
 
 	return string(content), nil
+}
+
+func readFileAndFilterString(inputFile, outputFile string) error {
+	file, err := os.Open(inputFile)
+	if err != nil {
+		return fmt.Errorf("failed to open file: %w", err)
+	}
+	defer file.Close()
+
+	outFile, err := os.Create(outputFile)
+	if err != nil {
+		return fmt.Errorf("failed to create output file: %w", err)
+	}
+	defer outFile.Close()
+
+	scanner := bufio.NewScanner(file)
+	writer := bufio.NewWriter(outFile)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		filtered := filterString(line)
+		_, err = writer.WriteString(filtered + "\n")
+		if err != nil {
+			return fmt.Errorf("failed to write to output file: %w", err)
+		}
+	}
+	if err := writer.Flush(); err != nil {
+		return fmt.Errorf("failed to flush writer: %w", err)
+	}
+
+	if err := scanner.Err(); err != nil {
+		return fmt.Errorf("error while reading file: %w", err)
+	}
+
+	return nil
+}
+
+func filterString(input string) string {
+	filtered := []rune{}
+	for _, r := range input {
+		if unicode.IsPrint(r) {
+			filtered = append(filtered, r)
+		}
+	}
+	return string(filtered)
 }
